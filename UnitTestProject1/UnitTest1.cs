a40758e5e0b6eb39e1313040883a313d7b69b51a
jc222fi <jc222fi@student.lnu.se>
2013-11-28 08:59:52 +0100

    Blackbox uppdatering

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void isEquilateralTest()
+        {
+            Triangle triangle = new Triangle(3.0, 3.0, 3.0);
+            Assert.IsTrue(triangle.isEquilateral());
+        }
+    }
+}
